89bdc138887159af6403598cf01fc7f253d1f9f7
Stephany Camacho-Lara <stephany_c19@yahoo.com>
2020-01-30 14:38:25 -0700

    fixed bugs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,32 +3,34 @@ const Workout = require("../models/workout.js")
 module.exports = function (app) {
 
     app.get("/api/workouts", function (req, res) {
-        Workout.find().then (data => {
-            res.json(data)
-        })
-        .catch(err =>{
-            res.json(err)
-        })
+        Workout.find()
+            .then(data => {
+                res.json(data)
+            })
+            .catch(err => {
+                res.json(err)
+            })
     });
 
-
     app.post("/api/workouts", function (req, res) {
-        console.log("POST");
         Workout.create({})
             .then(data => res.json(data))
             .catch(err => {
                 console.log("err", err)
                 res.json(err)
             })
-
     });
 
-    app.put("/api/workouts/:id", function (req, res) {
-        console.log("PUT", req.params, req.body);
-        // Workout.create({})
-        // .then(data=>res.json(data))
-
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("err", err)
+                res.json(err)
+            })
     });
-
-
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,17 @@
-
 var path = require("path");
 
-module.exports = function (app) {
-
-    app.get("/exercise", function (req, res) {
+module.exports = function (app){
+    
+    app.get("/exercise", function (req,res){
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 
-    // app.get("./workout.js", function(req,res){
-    //     res.sendFile(path.join(__dirname, "../public/workout.js"))
-    // });
-
-    app.get("/stats", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/stats.html"))
-    });
-
-    // app.get("./api.js", function(req,res){
-    //     res.sendFile(path.join(__dirname, "../public/api.js"))
-    // });
-
-    app.get("/", function (req, res) {
+    app.get("/", function(req,res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
+
+    app.get("/stats", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+
 }
